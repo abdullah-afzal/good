994dbccecab5929da776fad4e7cabb16844f2159
Abdullah Afzal <mr.abdullahafzal@gmail.com>
2022-05-30 09:02:54 +0500

    Add project files.

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> productImages { get; set; }
+        public DbSet<IPAddress> ipAddresses { get; set; }
+        public DbSet<StoreImage> storeImages { get; set; }
+        public DbSet<Tag> tags { get; set; }
+    }
+}
